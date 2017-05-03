294b5e56c114961f6bea750dd1bb3643ea85a8bf
pjehan <pierre.jehan@gmail.com>
2017-05-03 10:26:39 +0200

    Etape 3

--- index.php
+++ index.php
@@ -91,69 +91,23 @@ $liste_recettes[2] = array(
 
             <section class="row recipes">
 
-                <div class="col-md-4">
-                    <article>
-                        <a href="recipe-1.html">
-                            <img src="images/<?php echo $liste_recettes[0]["img"]; ?>" alt="<?php echo $liste_recettes[0]["title"]; ?>" class="img-responsive">
-                        </a>
-                        <h2><a href="recipe-1.html"><?php echo $liste_recettes[0]["title"]; ?></a></h2>
-                        <?php echo $liste_recettes[0]["description"]; ?>
-                        <footer>
-                            <div class="label"><a href="#" class="like"><i class="fa fa-heart"></i></a> <?php echo $liste_recettes[0]["nb_like"]; ?></div>
-                            <div class="label"><i class="fa fa-cutlery"></i> <?php echo $liste_recettes[0]["category"]; ?></div>
-                            <div class="label"><i class="fa fa-user"></i> <?php echo $liste_recettes[0]["user"]; ?></div>
-                            <div class="label"><i class="fa fa-calendar"></i> <?php echo $liste_recettes[0]["creationDate"]->format("j M Y"); ?></div>
-                        </footer>
-                    </article>
-                </div>
-
-                <div class="col-md-4">
-                    <article>
-                        <a href="#">
-                            <img src="images/recipe-2.jpg" alt="Poulet mariné" class="img-responsive">
-                        </a>
-                        <h2><a href="#">Pancake chocolat banane</a></h2>
-                        Délicieux pancake à la banane et au chocolat.
-                        <footer>
-                            <div class="label"><a href="#" class="like"><i class="fa fa-heart"></i></a> 42</div>
-                            <div class="label"><i class="fa fa-cutlery"></i> Desert</div>
-                            <div class="label"><i class="fa fa-user"></i> Etienne H.</div>
-                            <div class="label"><i class="fa fa-calendar"></i> 12 Sept. 2016</div>
-                        </footer>
-                    </article>
-                </div>
-
-                <div class="col-md-4">
-                    <article>
-                        <a href="#">
-                            <img src="images/recipe-3.jpg" alt="Poulet mariné" class="img-responsive">
-                        </a>
-                        <h2><a href="#">Poulet mariné</a></h2>
-                        Recette classique du poulet mariné...
-                        <footer>
-                            <div class="label"><a href="#" class="like"><i class="fa fa-heart"></i></a> 42</div>
-                            <div class="label"><i class="fa fa-cutlery"></i> Desert</div>
-                            <div class="label"><i class="fa fa-user"></i> Etienne H.</div>
-                            <div class="label"><i class="fa fa-calendar"></i> 12 Sept. 2016</div>
-                        </footer>
-                    </article>
-                </div>
-
-                <div class="col-md-4">
-                    <article>
-                        <a href="#">
-                            <img src="images/recipe-4.jpg" alt="Poulet mariné" class="img-responsive">
-                        </a>
-                        <h2><a href="#">Poulet mariné</a></h2>
-                        Recette classique du poulet mariné...
-                        <footer>
-                            <div class="label"><a href="#" class="like"><i class="fa fa-heart"></i></a> 42</div>
-                            <div class="label"><i class="fa fa-cutlery"></i> Desert</div>
-                            <div class="label"><i class="fa fa-user"></i> Etienne H.</div>
-                            <div class="label"><i class="fa fa-calendar"></i> 12 Sept. 2016</div>
-                        </footer>
-                    </article>
-                </div>
+                <?php foreach ($liste_recettes as $recette) : ?>
+                    <div class="col-md-4">
+                        <article>
+                            <a href="recipe-1.html">
+                                <img src="images/<?php echo $recette["img"]; ?>" alt="<?php echo $liste_recettes[0]["title"]; ?>" class="img-responsive">
+                            </a>
+                            <h2><a href="recipe-1.html"><?php echo $recette["title"]; ?></a></h2>
+                            <?php echo $recette["description"]; ?>
+                            <footer>
+                                <div class="label"><a href="#" class="like"><i class="fa fa-heart"></i></a> <?php echo $recette["nb_like"]; ?></div>
+                                <div class="label"><i class="fa fa-cutlery"></i> <?php echo $recette["category"]; ?></div>
+                                <div class="label"><i class="fa fa-user"></i> <?php echo $recette["user"]; ?></div>
+                                <div class="label"><i class="fa fa-calendar"></i> <?php echo $recette["creationDate"]->format("j M Y"); ?></div>
+                            </footer>
+                        </article>
+                    </div>
+                <?php endforeach; ?>
 
             </section>
 
